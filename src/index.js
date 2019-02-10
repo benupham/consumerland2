511d0c44a6228a9e52a9ee5e319dcfa7d9fcbe4a
Ben Upham <bcupham@gmail.com>
2019-02-09 18:40:30 -0800

    who knows

--- src/index.js
+++ src/index.js
@@ -4,12 +4,8 @@ import {forceCluster} from 'd3-force-cluster';
 
 const width = 960;
 const height = 500;
-const maxRadius = 12;
+const standardRadius = 12;
 const padding = .5; // separation between same-color nodes
-const clusterPadding = 6; // separation between different-color nodes
-
-var n = 200, // total number of circles
-    m = 10; // number of distinct clusters
 
 var color = d3.scaleOrdinal(d3.schemeCategory20)
     .domain(d3.range(20));
@@ -18,6 +14,7 @@ var color = d3.scaleOrdinal(d3.schemeCategory20)
 d3.json("./data/testd3dataNoSalesDept.json", function(error, root) {
 
   // create the circle pack algorithm
+  // only used once to optimize initial clustering
   var pack = d3.pack()
     .size([(width), (height)])
     .padding(0);  
@@ -30,31 +27,43 @@ d3.json("./data/testd3dataNoSalesDept.json", function(error, root) {
   // circle pack the data    
   var thePack = pack(root);  
 
-  var depts = [];
-  var subdepts = [];
+  const depts = [];
+  const subdepts = [];
+  const brands = [];
+  const products = [];
 
-  // extract the circle-packed subdepartment nodes
+  // extract the levels of nodes for use later
+  // but not sure why I'm extracting from thePack for anything below subdept
   thePack.children.forEach(d => {
     depts.push(d);
     d.children.forEach(sd => {
       subdepts.push(sd);
+      sd.children.forEach(b => {
+        brands.push(b);
+        b.children.forEach(p => {
+          products.push(p);
+        })
+      })
     });
   });
-  
+
+  // an object for all clusters at all depth levels
   var clustersObj = {};
 
-  // create the nodes, and assign the "largest" for each dept to clustersObj
-  var nodes = subdepts.map(function(sd) {
-    const id = sd.data.id;
-    const i = sd.parent.data.name;
-    const r = 12;
-    const d = {cluster: i, radius: r, x: sd.x, y: sd.y, id: id};
+  // assign nodes to a cluster
+  function clusterize (n) {
+    n.radius = standardRadius;
+    // each child is assigned to a cluster named after its parent
+    n.cluster = n.parent.data.name;
 
-    // this is only really relevant if r is not the same for every node
-    if (!clustersObj[i] || (r > clustersObj[i].radius)) clustersObj[i] = d;
-    return d;
-  });
-  console.log('nodes',nodes)
+    // each cluster's value is the largest child
+    if (!clustersObj[n.cluster] || (n.data.value > clustersObj[n.cluster].value)) clustersObj[n.cluster] = n;
+
+  }
+
+  subdepts.forEach(sd => clusterize(sd));  
+  brands.forEach(b => clusterize(b));
+  products.forEach(p => clusterize(p));
 
   var simulation = d3.forceSimulation()
     // keep entire simulation balanced around screen center
@@ -77,44 +86,53 @@ d3.json("./data/testd3dataNoSalesDept.json", function(error, root) {
       .iterations(5))
 
     .on('tick', layoutTick)
-    .nodes(nodes);
+    .nodes(subdepts);
     
+  // add svg and apply zooming  
   var svg = d3.select('body').append('svg')
       .attr('width', width)
       .attr('height', height)
       .call(d3.zoom().on("zoom", function () {
         svg.attr("transform", d3.event.transform)
-        }))
+      }))
       .append("g");
 
   var node = svg.selectAll('circle')
-    .data(nodes)
+    .data(subdepts)
     .enter().append('circle')
-      .style('fill', function (d) { return color(d.cluster); })
-      .call(d3.drag()
-        .on('start', dragstarted)
-        .on('drag', dragged)
-        .on('end', dragended)
-      );
+      .style('fill', function (d) { 
+        return color(d.cluster); 
+      })
+      .on('click', clicked);
+  
+    function clicked(parentData) {
+      
+      brands.forEach(b => {
+        if (b.cluster == parentData.data.name) {
+          b.x = parentData.x;
+          b.y = parentData.y;
+          subdepts.push(b);
+        }
+      })
+      node
+        .data(subdepts)
+        .enter().append('circle')
+        .style('fill', function (d) { 
+          return color(d.cluster); 
+        })
+      console.log('OTHER',node)
+      simulation.nodes(subdepts).alphaTarget(0.3).restart();
 
-  function dragstarted (d) {
-    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
+    }
+    
+  function layoutTick (e) {
+    console.log(node)
+    node
+      .attr('cx', function (d) { return d.x; })
+      .attr('cy', function (d) { return d.y; })
+      .attr('r', function (d) { return d.radius; });
   }
 
-  function dragged (d) {
-    // position of node is "fixed" to position of mouse
-    // so it is not acted upon on each tick
-    d.fx = d3.event.x;
-    d.fy = d3.event.y;
-  }
-
-  function dragended (d) {
-    if (!d3.event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
 
   // runs once on load to provide smooth transition
   var transitionTime = 3000;
@@ -123,12 +141,4 @@ d3.json("./data/testd3dataNoSalesDept.json", function(error, root) {
     simulation.force('collide').strength(Math.pow(dt, 2) * 0.7);
     if (dt >= 1.0) t.stop();
   });
-    
-  function layoutTick (e) {
-    node
-      .attr('cx', function (d) { return d.x; })
-      .attr('cy', function (d) { return d.y; })
-      .attr('r', function (d) { return d.radius; });
-  }
-
 })
